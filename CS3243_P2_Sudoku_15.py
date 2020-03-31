b3c386284f585ce3b00735cc2ec25df09577f8e1
weikiat97 <jerryloh97@hotmail.com>
2020-04-01 01:40:23 +0800

    Fixed Sudoku.

--- CS3243_P2_Sudoku_15.py
+++ CS3243_P2_Sudoku_15.py
@@ -1,306 +1,124 @@
 import sys
 import copy
 
-# Running script: given code can be run with the command:
-# python file.py, ./path/to/init_state.txt ./output/output.txt
-
 class Sudoku(object):
     def __init__(self, puzzle):
-        # you may add more attributes if you need
         self.puzzle = puzzle # self.puzzle is a list of lists
-        self.fixed = set()
         self.ans = copy.deepcopy(puzzle) # self.ans is a list of lists
-        # Each node is a cell on the sudoku
-        # When we add the arcs into the ac3, the node-node refers to the relation between two cells
 
     def solve(self):
-        # TODO: Write your code here
-        self.initialise()
+        self.initialise() 
         self.backtrack(puzzle)
-        # self.ans is a list of lists
+        self.convert_to_output()
         return self.ans
 
+    # Initialise all the values in the puzzle as a set of values.
+    # If there is a pre-existing value, we set it to be one single value inside the set.
+    # If the value is 0, we provide it with a set of all possible values from 1-9.
     def initialise(self):
         for row in range(9):
             for col in range(9):
                 if self.puzzle[row][col] != 0:
                     set_values = {puzzle[row][col]}
                     puzzle[row][col] = set_values
-                    self.fixed.add((row, col))
-                #else:
-                #    puzzle[row][col] = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+                else:
+                    puzzle[row][col] = {1, 2, 3, 4, 5, 6, 7, 8, 9}
 
+    # Converts from values in a set to values for output.
+    def convert_to_output(self):
         for row in range(9):
             for col in range(9):
-                if self.puzzle[row][col] == 0: # Find the empty cells
-                    possible_values = {1, 2, 3, 4, 5, 6, 7, 8, 9}
-                    for same_row_col in range(9):
-                        if (row, same_row_col) in self.fixed:
-                            value_to_remove = list(puzzle[row][same_row_col])[0]
-                            if value_to_remove in possible_values:
-                                possible_values.remove(value_to_remove)
-                    for same_col_row in range(9):
-                        if (same_col_row, col) in self.fixed:
-                            value_to_remove = list(puzzle[same_col_row][col])[0]
-                            if value_to_remove in possible_values:
-                                possible_values.remove(value_to_remove)                    
-                    top_row_in_box = int(row / 3) * 3
-                    top_col_in_box = int(col / 3) * 3
-                    for same_box_row_value in range(top_row_in_box, top_row_in_box + 3):
-                        for same_box_col_value in range(top_col_in_box, top_col_in_box + 3):
-                             if (same_box_row_value, same_box_col_value) in self.fixed:
-                                value_to_remove = list(puzzle[same_box_row_value][same_box_col_value])[0]
-                                if value_to_remove in possible_values:
-                                        possible_values.remove(value_to_remove)
-                    self.puzzle[row][col] = possible_values
-                    
-                    #for same_row_value in range(9):
-                     #   print("Same row value:")
-                      #  print(same_row_value)
-                       # if self.puzzle[row][same_row_value] in possible_values:
-                        #    print("Removing above value")
-                         #   possible_values.remove(self.puzzle[row][same_row_value])
-                    #for same_col_value in range(9):
-                     #   if self.puzzle[same_col_value][col] in possible_values:
-                      #      possible_values.remove(self.puzzle[same_col_value][col])
-                    #top_row_in_box = int(row / 3) * 3
-                    #top_col_in_box = int(col / 3) * 3
-                    #for same_box_row_value in range(top_row_in_box, top_row_in_box + 3):
-                     #   for same_box_col_value in range(top_col_in_box, top_col_in_box + 3):
-                      #      if self.puzzle[same_box_row_value][same_box_col_value] in possible_values:
-                       #         possible_values.remove(puzzle[same_box_row_value][same_box_col_value])
-                    #self.puzzle[row][col] = possible_values
-                    print("Initialising")
-                    print((row, col))
-                    print(self.puzzle[row][col])
-                #else :
-                 #   set_values = {puzzle[row][col]}
-                  #  puzzle[row][col] = set_values
-                   # self.fixed.add((row, col))
+                for value in self.puzzle[row][col]:
+                    self.ans[row][col] = value
 
-    # In this problem, we are ensured that the puzzle is valid and well-formed.
-    def find_mrv(self):
-        mrv = 10
-        mrv_position = None
-        for row in range(9):
-            for col in range(9):
-                possible_values = puzzle[row][col]
-                if len(possible_values) < mrv and (row, col) not in self.fixed:
-                    mrv = len(possible_values)
-                    mrv_position = (row, col)
-        return mrv_position
-                    
-    # Model the problem such that we are able to see the available domains
-    # Put it as a a dictionary, key is a tuple of the position, the values is a list of the possible values
-    # At first, we will run ac3 to find the initial possible domains of everything. When we do this, we can find the smallest 
-    # domain item as a variable and use it next (Most constrained value MRV Heuristics)
-    # Select this variable and assign it the first value in the domain
-    # Repeats ac3 to revise and find all possible domains again. If any of this domain becomes empty, failure, choose another value
-    # else continue until it is all done. Do this is a recursion call to backtrack again with the new puzzle 
-
-    
-    # Does backtrack first
-    # If assignment is complete, return assignment
-    # Use one of the heuristic to select the first variable
-    # Dict is a dictionary of all the arcs and its possible domain values
+    # Checks if every cell in the puzzle is ac3.
+    # If all values are assigned, we return the puzzle.
+    # If not, it chooses the position to set the new value based on the Minimum-Remaining-Values (MRV) heuristic.
+    # For each possible value in this position, we try it and see if it returns a consistent solution.
     def backtrack(self, puzzle):
-        if self.puzzle_is_completed(puzzle):
-            #print("Checking is puzzle is complete")
-            return puzzle
-        mrv_row, mrv_col = self.find_mrv()
-        mrv_values = puzzle[mrv_row][mrv_col]
-        i = 0
+        for row in range(9):
+            for col in range(9):
+                if not self.ac3(puzzle, (row, col)):
+                    return False
+        mrv_row, mrv_col = self.find_mrv(puzzle)
+        if mrv_row == -1 and mrv_col == -1:
+            self.puzzle = puzzle
+            return True
+        mrv_values = puzzle[mrv_row][mrv_col].copy()
         for value in mrv_values:
-            i += 1
-            print("im here for the")
-            print(i)
-            print("times")
-            print("value is")
-            print(value)
-            current_value_failed = False
-            #print("Iterating through all the values in MRV values. MRV position:")
-            #print(mrv_row, mrv_col)
-            #print("Current Value:")
-            #print(value)
-            self.check_consistent(puzzle, (mrv_row, mrv_col), value)
             current_puzzle = copy.deepcopy(puzzle)
             current_puzzle[mrv_row][mrv_col] = {value}
-            print("What is the current puzzle value assigned?")
-            print((mrv_row,mrv_col))
-            print(current_puzzle[mrv_row][mrv_col])
-            if not self.ac3(current_puzzle, (mrv_row, mrv_col)):
-                continue
-            #for same_row_col in range(9):
-             #   if mrv_col != same_row_col:
-              #      if not self.ac3(current_puzzle, (mrv_row, same_row_col)):
-               #         current_value_failed = True
-            #if current_value_failed:
-             #   continue
-            #for same_col_row in range(9):
-             #   if mrv_row != same_col_row:
-              #      if not self.ac3(current_puzzle, (same_col_row, mrv_col)):
-               #         current_value_failed = True
-            #if current_value_failed:
-             #   continue
-            #top_row_in_box = int(mrv_row / 3) * 3
-            #top_col_in_box = int(mrv_col / 3) * 3
-            #for same_box_row_value in range(top_row_in_box, top_row_in_box + 3):
-             #   for same_box_col_value in range(top_col_in_box, top_col_in_box + 3):
-              #      if same_box_row_value != mrv_row and same_box_col_value != mrv_col:
-               #         if not self.ac3(current_puzzle, (same_box_row_value, same_box_col_value)):
-                #            current_value_failed = True
-            #if current_value_failed:
-             #   continue
-            print("True that it is ac3")
-            new_puzzle = copy.deepcopy(current_puzzle)
-            if self.backtrack(new_puzzle):
+            if self.backtrack(current_puzzle):
                 return True
         return False
 
-    def check_consistent(self, puzzle, position, value):
-        mrv_row, mrv_col = position
-        for same_row_col in range(9):
-            if mrv_col != same_row_col:
-                if len(puzzle[mrv_row][same_row_col]) == 1 and value in puzzle[mrv_row][same_row_col]:
-                    return False
-        for same_col_row in range(9):
-            if mrv_row != same_col_row:
-                if len(puzzle[same_col_row][mrv_col]) == 1 and value in puzzle[same_col_row][mrv_col]:
-                    return False
-        top_row_in_box = int(mrv_row / 3) * 3
-        top_col_in_box = int(mrv_col / 3) * 3
-        for same_box_row_value in range(top_row_in_box, top_row_in_box + 3):
-            for same_box_col_value in range(top_col_in_box, top_col_in_box + 3):
-                if same_box_row_value != mrv_row and same_box_col_value != mrv_col:
-                    if len(puzzle[same_box_row_value][same_box_col_value]) == 1 and value in puzzle[same_box_row_value][same_box_col_value]:
-                        return False
-        return True
-
-    # When we call this function, there are already previous checks for whether the assignment is correct.
-    # Hence, no need to do so again
-    def puzzle_is_completed(self, puzzle):
+    # In this problem, we are ensured that the puzzle is valid and well-formed.
+    # Hence, we will just look through the given puzzles and count the number of possible values in it for our heuristic
+    # If there are no values that are lesser than 10 and more than 1, we have found a solution, as every value is assigned.
+    def find_mrv(self, puzzle):
+        mrv = 10
+        mrv_position = (-1, -1)
         for row in range(9):
             for col in range(9):
-                if len(puzzle[row][col]) != 1:
-                    return False
-        return True
-
-    # Over here, we have a queue of all the possible arcs (i.e. position of the current box we are looking at) (Inference)
-    # Pop first item from queue, 
-    def ac3(self, puzzle, position):
-        queue = []
+                possible_values = puzzle[row][col]
+                if len(possible_values) < mrv and len(possible_values) != 1:
+                    mrv = len(possible_values)
+                    mrv_position = (row, col)
+        return mrv_position
+                
+    # Helper function that returns all the cells that are connected (same row / same col / same box) to the given position, as a list.
+    def get_connecting_cells_positions(self, puzzle, position):
         row, col = position
-        for same_row_col in range(9):
-            if col != same_row_col:
-                queue.append((row, same_row_col))
-        for same_col_row in range(9):
-            if row != same_col_row:
-                queue.append((same_col_row, col))
-        top_row_in_box = int(row / 3) * 3
+        output = []
+        for col_value in range(9): # Items in same row
+            if col_value != col:
+                output.append((row, col_value))
+        for row_value in range(9): # Items in same col
+            if row_value != row:
+                output.append((row_value, col))
+        top_row_in_box = int(row / 3) * 3 # Items in the same box
         top_col_in_box = int(col / 3) * 3
         for same_box_row_value in range(top_row_in_box, top_row_in_box + 3):
             for same_box_col_value in range(top_col_in_box, top_col_in_box + 3):
                 if same_box_row_value != row and same_box_col_value != col:
-                    queue.append((same_box_row_value, same_box_col_value))
-        #print("After appending at AC3?")
+                    output.append((same_box_row_value, same_box_col_value))
+        return output
+
+
+    # We will append the position, and if this position's domain is revised, we will add all the connecting cells back into the queue.
+    # This continues until the domains of all the cells cannot be reduced anymore.
+    def ac3(self, puzzle, position):
+        queue = []
+        queue.append(position)
         while len(queue) != 0:
-            print("Getting item in queue")
-            current_position = queue.pop()
-            print(current_position)
-            row, col = current_position
-            #print(puzzle[row][col])
+            current_position = queue.pop(0)
             if self.revise(puzzle, current_position):
-                print("Has been revised")
-                row, col = current_position
-                print(puzzle[row][col])
-                if len(puzzle[row][col]) == 0: # Means that there is a wrong domain assignment
-                    print("Length is 0")
-                    return False
-                for col_value in range(9): # Items in same row
-                    if col_value != col:
-                        #print("Appending col_value")
-                        queue.append((row, col_value))
-                for row_value in range(9): # Items in same col
-                    if row_value != row:
-                        #print("Appending row_value")
-                        queue.append((row_value, col))
-                top_row_in_box = int(row / 3) * 3 # Items in the same box
-                top_col_in_box = int(col / 3) * 3
-                for same_box_row_value in range(top_row_in_box, top_row_in_box + 3):
-                    for same_box_col_value in range(top_col_in_box, top_col_in_box + 3):
-                        if same_box_row_value != row and same_box_col_value != col:
-                            #print("Appending box value")
-                            queue.append((same_box_row_value, same_box_col_value))
+                connecting_cells_positions = self.get_connecting_cells_positions(puzzle, current_position)
+                for neighbouring_position in connecting_cells_positions:
+                    queue.append(neighbouring_position)
         return True
             
     
-    # Check to see if row / cols / boxes satisfy the constraint
+    # Check to see if any of the connecting cell's domain consists of only a certain value of the cell we are looking at.
+    # If it has, this means that this particular connecting cell's value is fixed.
+    # Hence, we know that we can remove this value out of the cell we are looking at, as we cannot assign it that value regardless.
     def revise(self, puzzle, position):
         is_revised = False
         row, col = position
         values = puzzle[row][col]
         values_to_remove = set()
-        print("Start of revise")
-        print(values)
+        connecting_cells_positions = self.get_connecting_cells_positions(puzzle, position)
         for value in values:
-            value_found = False
-            for col_value in range(9): # Items in the same row
-                if col_value != col:
-                    #print("I'm rly here")
-                    if len(puzzle[row][col_value]) == 1 and value in puzzle[row][col_value]:
-                        print("At revise col value")
-                        print(value)
-                        print("Row")
-                        print(row)
-                        print("Col")
-                        print(col_value)
-                        print("item inside is ")
-                        print(puzzle[row][col_value])
-                        values_to_remove.add(value)
-                        break
-
-            for row_value in range(9): # Items in the same col
-                if row_value != row:
-                    if len(puzzle[row_value][col]) == 1 and value in puzzle[row_value][col]:
-                        print("At revise row value")
-                        print(value)
-                        values_to_remove.add(value)
-                        break
-            #if value_found:
-            #    break        
-
-            top_row_in_box = int(row / 3) * 3 # Items in the same box
-            #print("Top row in box:")
-            #print(top_row_in_box)
-           # print("Top col in box:")
-            top_col_in_box = int(col / 3) * 3
-            #print(top_col_in_box)
-            for same_box_row_value in range(top_row_in_box, top_row_in_box + 3):
-                for same_box_col_value in range(top_col_in_box, top_col_in_box + 3):
-                    if same_box_row_value != row and same_box_col_value != col:
-                        #print(same_box_row_value, same_box_col_value)
-                        #print(puzzle[same_box_row_value][same_box_col_value])
-                        if len(puzzle[same_box_row_value][same_box_col_value]) == 1 and value in puzzle[same_box_row_value][same_box_col_value]:
-                            values_to_remove.add(value)
-                            print("At revise box value")
-                            print(value)
-                            break
-
+            for neighbouring_position in connecting_cells_positions:
+                neighbour_row, neighbour_col = neighbouring_position
+                if len(puzzle[neighbour_row][neighbour_col]) == 1 and value in puzzle[neighbour_row][neighbour_col]:
+                    values_to_remove.add(value)
+                    break
         for value in values_to_remove:
             is_revised = True
-            #print("Removing")
-            #print(value)
-            values.remove(value)
-       # print("Values remaining after revision")
-       # print(position)
-      #  print(values)
+            puzzle[row][col].remove(value)
         return is_revised
-
-
-    # you may add more classes/functions if you think is useful
-    # However, ensure all the classes/functions are in this file ONLY
-    # Note that our evaluation scripts only call the solve method.
-    # Any other methods that you write should be used within the solve() method.
+        
 
 if __name__ == "__main__":
     # STRICTLY do NOT modify the code in the main function here
